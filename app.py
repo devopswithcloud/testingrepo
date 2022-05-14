15e6e45f71a08f419e86493f1c7cb07cb3d20b92
devopswithcloud <65442863+devopswithcloud@users.noreply.github.com>
2022-05-14 18:16:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Welcome to Openshift Training"
+    return "Welcome to Openshift Training By Siva!!!!"
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
